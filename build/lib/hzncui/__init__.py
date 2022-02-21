8e96622b39b4964cf1fbd29286cc195eddccad44
Joe Pearson <joewxboy@users.noreply.github.com>
2022-02-21 17:06:49 -0500

    secondary menu
    Signed-off-by: Joe Pearson <joewxboy@users.noreply.github.com>

--- build/lib/hzncui/__init__.py
+++ build/lib/hzncui/__init__.py
@@ -1,4 +1,4 @@
-"""Main TUI implementation for Open Horizon CUI
+"""Main TUI implementation for Open Horizon Management Hub CUI
 
 Author: Joe Pearson  
 Created: 2022-02-19
@@ -7,31 +7,52 @@ Created: 2022-02-19
 
 import py_cui, requests, json, os
 
-__version__ = 'v0.0.1'
+__version__ = '0.0.1'
 
 class hzncuiApp:
 
-    def __init__(self, master):
+    def __init__(self, parent):
 
-        self.master = master
+        self.parent = parent
+
+        # read Open Horizon configuration from environment variables
+
         hzn_org_id = os.environ['HZN_ORG_ID']
         exchange_user_admin_pw = os.environ['EXCHANGE_USER_ADMIN_PW']
         hzn_exchange_url = os.environ['HZN_EXCHANGE_URL']
 
+        # retrieve data from the configured Open Horizon Exchange
+
         r = requests.get(f'{hzn_exchange_url}/orgs/{hzn_org_id}/node-details', auth=(f'{hzn_org_id}/admin', exchange_user_admin_pw))
         data = json.loads(r.text)
 
-        self.edge_nodes_menu = self.master.add_scroll_menu('Select edge node', 0, 0, row_span=1, column_span=3)
-        self.edge_nodes_menu.set_selected_color(py_cui.BLACK_ON_YELLOW)
-        
+        li = ['Nodes', 'Services', 'Patterns', 'Policies', 'Orgs', 'Users']
+        self.primary_menu = self.parent.add_scroll_menu('1. Choose an item', 0, 0, row_span=1, column_span=1)
+        self.primary_menu.set_selected_color(py_cui.BLACK_ON_YELLOW)
+        self.primary_menu.add_item_list(li)
+
+        # create the CUI widget for displaying node details
+
+        self.secondary_menu = self.parent.add_scroll_menu('2. Choose a node to see details', 0, 1, row_span=1, column_span=2)
+        self.secondary_menu.set_selected_color(py_cui.BLACK_ON_YELLOW)
+
+        # create a list of IDs from the list of edge node dicts
+
         lid = []
         for d in data:
             lid.append(d["id"])
 
-        self.edge_nodes_menu.add_item_list(lid)
+        # put the list of IDs into the node details CUI widget
+
+        self.secondary_menu.add_item_list(lid)
+
+        # create the detail box
+
+        current_node = self.secondary_menu.get()
+        self.detail_box = self.parent.add_block_label(f'3. Details for node {current_node}', 1, 0, row_span=1, column_span=3, padx=0, pady=0)
 
 def main():
     root = py_cui.PyCUI(2, 3)
-    root.set_title(f'Open Horizon CUI {__version__}')
+    root.set_title(f'Open Horizon Management Hub CUI v{__version__}')
     wrapper =  hzncuiApp(root)
     root.start()
--- hzncui/__init__.py
+++ hzncui/__init__.py
@@ -1,4 +1,4 @@
-"""Main TUI implementation for Open Horizon CUI
+"""Main TUI implementation for Open Horizon Management Hub CUI
 
 Author: Joe Pearson  
 Created: 2022-02-19
@@ -7,7 +7,7 @@ Created: 2022-02-19
 
 import py_cui, requests, json, os
 
-__version__ = 'v0.0.1'
+__version__ = '0.0.1'
 
 class hzncuiApp:
 
@@ -26,10 +26,15 @@ class hzncuiApp:
         r = requests.get(f'{hzn_exchange_url}/orgs/{hzn_org_id}/node-details', auth=(f'{hzn_org_id}/admin', exchange_user_admin_pw))
         data = json.loads(r.text)
 
+        li = ['Nodes', 'Services', 'Patterns', 'Policies', 'Orgs', 'Users']
+        self.primary_menu = self.parent.add_scroll_menu('1. Choose an item', 0, 0, row_span=1, column_span=1)
+        self.primary_menu.set_selected_color(py_cui.BLACK_ON_YELLOW)
+        self.primary_menu.add_item_list(li)
+
         # create the CUI widget for displaying node details
 
-        self.edge_nodes_menu = self.parent.add_scroll_menu('Select edge node', 0, 0, row_span=1, column_span=3)
-        self.edge_nodes_menu.set_selected_color(py_cui.BLACK_ON_YELLOW)
+        self.secondary_menu = self.parent.add_scroll_menu('2. Choose a node to see details', 0, 1, row_span=1, column_span=2)
+        self.secondary_menu.set_selected_color(py_cui.BLACK_ON_YELLOW)
 
         # create a list of IDs from the list of edge node dicts
 
@@ -39,10 +44,15 @@ class hzncuiApp:
 
         # put the list of IDs into the node details CUI widget
 
-        self.edge_nodes_menu.add_item_list(lid)
+        self.secondary_menu.add_item_list(lid)
+
+        # create the detail box
+
+        current_node = self.secondary_menu.get()
+        self.detail_box = self.parent.add_block_label(f'3. Details for node {current_node}', 1, 0, row_span=1, column_span=3, padx=0, pady=0)
 
 def main():
     root = py_cui.PyCUI(2, 3)
-    root.set_title(f'Open Horizon CUI v{__version__}')
+    root.set_title(f'Open Horizon Management Hub CUI v{__version__}')
     wrapper =  hzncuiApp(root)
     root.start()
